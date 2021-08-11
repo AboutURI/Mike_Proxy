6f0d152e0203d37feb71f8afbf49cff666d20de9
Michael Gallien <Michael.Gallien2@gmail.com>
2021-08-10 19:10:12 -0700

    Deploed to EC2, need to stress test

--- server/server.js
+++ server/server.js
@@ -3,13 +3,64 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+require('newrelic');
+
+const serverOne = 'http://ec2-13-57-225-97.us-west-1.compute.amazonaws.com:3004/'
 
 app.use(shrinkRay());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cors());
 
 app.use('/course', (req, res) => {
+  // console.log('In course route')
   res.sendFile('index.html', {root: 'public'});
 });
+
+app.all("/sidebar", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
+});
+
+app.all("/price", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
+});
+
+app.all("/previewVideo", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
+});
+
+// app.use('/', (req, res) => {
+//   res.send('Hello World');
+// })
+
+
+// var express  = require('express');
+// var app      = express();
+// var httpProxy = require('http-proxy');
+// var apiProxy = httpProxy.createProxyServer();
+// var serverOne = 'http://localhost:4001',
+//     ServerTwo = 'http://localhost:4002',
+//     ServerThree = 'http://localhost:4003';
+ 
+// app.all("/photogallery/", function(req, res) {
+//     console.log('redirecting to Server1');
+//     apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
+// });
+
+// app.all("/overview/", function(req, res) {
+//     console.log('redirecting to Server2');
+//     apiProxy.web(req, res, {target: ServerTwo});
+// });
+
+// app.all("/reviews/", function(req, res) {
+//     console.log('redirecting to Server3');
+//     apiProxy.web(req, res, {target: ServerThree});
+// });
+
+// app.listen(4000);
 
 module.exports = app;
--- server/start.js
+++ server/start.js
@@ -2,10 +2,16 @@ const app = require('./server.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 6012;
-const host = process.env.PRIVATE_HOST || 'localhost'
 
-const serverInstance = app.listen(port, host, () => {
-  console.log(`Listening at http://${host}:${port}`)
+// NEED TO KEEP HOST UNSPECIFIED TO RUN EXPRESS ON EC2
+// const host = process.env.PRIVATE_HOST || 'localhost'
+
+// const serverInstance = app.listen(port, host, () => {
+//   console.log(`Listening at http://${host}:${port}`)
+// });
+
+const serverInstance = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
 });
 
 const closeServer = () => {
