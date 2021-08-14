9590633ccbc470050b7f967d5389d04dc68cac12
Michael Gallien <Michael.Gallien2@gmail.com>
2021-08-13 20:44:21 -0700

    Added Caching

--- server/server.js
+++ server/server.js
@@ -5,19 +5,38 @@ const app = express();
 const cors = require('cors');
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
+const redis = require('redis');
 require('newrelic');
 
-const serverOne = 'http://ec2-13-57-225-97.us-west-1.compute.amazonaws.com:3004/';
+// const serverOne = 'http://ec2-13-57-225-97.us-west-1.compute.amazonaws.com:3004/';
 const nginx = 'http://ec2-54-219-150-232.us-west-1.compute.amazonaws.com/';
+const REDIS_PORT = process.env.PORT || 6379;
 
 app.use(shrinkRay());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cors());
 
-// app.use('/course', (req, res) => {
-//   // console.log('In course route')
-//   res.sendFile('index.html', {root: 'public'});
-// });
+app.use('/loaderio-a85629e90fe9bbed9f6eb457c09a1b34', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'loaderio-a85629e90fe9bbed9f6eb457c09a1b34.txt'));
+});
+
+//Create Cache
+const client = redis.createClient(REDIS_PORT);
+
+//Caching Function
+function cache(req, res, next) {
+  const { Id } = req.query.courseId;
+
+  client.get(Id, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(data);
+    } else {
+      next();
+    }
+  });
+}
 
 app.all("/sidebar", function(req, res) {
   console.log('redirecting to Server1');
@@ -33,35 +52,5 @@ app.all("/previewVideo", function(req, res) {
   console.log('redirecting to Server1');
   apiProxy.web(req, res, {target: nginx, changeOrigin: true});
 });
-
-// app.use('/', (req, res) => {
-//   res.send('Hello World');
-// })
-
-
-// var express  = require('express');
-// var app      = express();
-// var httpProxy = require('http-proxy');
-// var apiProxy = httpProxy.createProxyServer();
-// var serverOne = 'http://localhost:4001',
-//     ServerTwo = 'http://localhost:4002',
-//     ServerThree = 'http://localhost:4003';
- 
-// app.all("/photogallery/", function(req, res) {
-//     console.log('redirecting to Server1');
-//     apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
-// });
-
-// app.all("/overview/", function(req, res) {
-//     console.log('redirecting to Server2');
-//     apiProxy.web(req, res, {target: ServerTwo});
-// });
-
-// app.all("/reviews/", function(req, res) {
-//     console.log('redirecting to Server3');
-//     apiProxy.web(req, res, {target: ServerThree});
-// });
-
-// app.listen(4000);
 
 module.exports = app;
